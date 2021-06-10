09829c3fe77c0eee26993d76715cf7c56732980d
Prince Sinha <prince@Princes-MacBook-Air.local>
2021-06-10 22:39:51 +0530

    Add description field and save button
    Resolves #2

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,6 +1,25 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {disabled: true, description: props.description};
+
+    this.handleDescriptionChange = this.handleDescriptionChange.bind(this)
+  }
+
+  // Handle Description change
+  handleDescriptionChange(event) {
+    let prevDescription = this.props.description,
+      currentDescription = event.target.value;
+
+    if( prevDescription !== currentDescription ){
+      this.setState({...this.state, disabled: false});
+    }
+
+    this.props.handleDescriptionChange(this.props.counter, currentDescription)
+  }
+
   render() {
     return (
       <div>
@@ -10,7 +29,7 @@ class Counter extends Component {
               {this.formatCount()}
             </span>
           </div>
-          <div className="col-md-4">
+          <div className="col-md-6">
             <button
               className="btn btn-secondary"
               onClick={() => this.props.onIncrement(this.props.counter)}
@@ -30,6 +49,19 @@ class Counter extends Component {
             >
               <i className="fa fa-trash-o" aria-hidden="true" />
             </button>
+            <input type="text" className="form-control d-inline w-50 m-2 description" placeholder="Description">
+            </input>
+            <button
+              className={`btn d-inline 
+                ${ !this.state.disabled ? "btn-primary" : "" } 
+                ${ this.state.disabled ? "btn-light" : "" }`
+              }
+              value={this.state.description}
+              onChange={(evt) => this.handleDescriptionChange(evt)}
+            >
+              Save
+            </button>
+
           </div>
         </div>
       </div>
@@ -46,6 +78,7 @@ class Counter extends Component {
     const { value } = this.props.counter;
     return value === 0 ? "Zero" : value;
   };
+
 }
 
 export default Counter;
